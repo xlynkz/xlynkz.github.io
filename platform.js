a007e6dd6afef7ef34724cce40f8c455939cf585
Fastur <Fastur@users.noreply.github.com>
2015-09-30 08:31:49 -0500

    Update and rename smile.js to platform.js

---
+++ platform.js
@@ -0,0 +1,5 @@
+<script src="http://cdn.auth0.com/js/lock-7.min.js"></script>
+<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<script src="https://cdn.firebase.com/js/client/2.3.0/firebase.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/algoliasearch/3.8.1/algoliasearch.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/annyang/2.0.0/annyang.min.js"></script>
